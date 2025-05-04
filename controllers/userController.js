ee1b6083d4cd71dd3b49f9387d9a431525c27f20
hazrat <hazrat17016@gmail.com>
2025-05-04 10:56:47 +0600

    user can update their profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
 exports.getAllUsers = async (req, res,  next) => {
     try {
@@ -7,6 +8,35 @@ exports.getAllUsers = async (req, res,  next) => {
         res.status(200).json(users);
     }catch (error) {
         console.error("Error fetching users:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+exports.updateUser = async (req, res, next) => {
+    const  userId  = req.params.userId;
+    let { name, email, password } = req.body;
+
+    try {
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        password = await bcrypt.hash(password, 11);
+        const updatedUser = await User.findByIdAndUpdate(
+            userId,
+            { name, email, password },
+            { new: true }
+        );
+
+        if (!updatedUser) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        res.status(200).json({
+            message: "User updated successfully",updatedUser
+        });
+    } catch (error) {
+        console.error("Error updating user:", error);
         res.status(500).json({ message: "Internal server error" });
     }
 }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,8 @@
-const { getAllUsers } = require("../controllers/userController");
+const { getAllUsers, updateUser } = require("../controllers/userController");
 const { authMiddleware } = require("../middlewares/auth");
 const userRoute = require("express").Router();
 
 userRoute.get("/", authMiddleware, getAllUsers);
+userRoute.put("/:userId", authMiddleware, updateUser);
 
 exports.userRoute = userRoute;;
