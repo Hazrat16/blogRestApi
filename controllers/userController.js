b4c56ecbba976402d62c0abc5940964338f5e972
hazrat <hazrat17016@gmail.com>
2025-05-04 11:35:34 +0600

    user can deleted and fetched by id

--- controllers/userController.js
+++ controllers/userController.js
@@ -40,3 +40,40 @@ exports.updateUser = async (req, res, next) => {
         res.status(500).json({ message: "Internal server error" });
     }
 }
+
+exports.deleteUser = async (req, res, next) => {
+    const userId = req.params.userId;
+
+    try {
+        const deletedUser = await User.findByIdAndDelete(userId);
+
+        if (!deletedUser) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        res.status(200).json({
+            message: "User deleted successfully",
+            deletedUser,
+        });
+    } catch (error) {
+        console.error("Error deleting user:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
+exports.getUserById = async (req, res, next) => {
+    const userId = req.params.userId;
+
+    try {
+        const user = await User.findById(userId);
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        res.status(200).json(user);
+    }
+    catch (error) {
+        console.error("Error fetching user:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,10 @@
-const { getAllUsers, updateUser } = require("../controllers/userController");
+const { getAllUsers, updateUser, deleteUser, getUserById } = require("../controllers/userController");
 const { authMiddleware } = require("../middlewares/auth");
 const userRoute = require("express").Router();
 
 userRoute.get("/", authMiddleware, getAllUsers);
 userRoute.put("/:userId", authMiddleware, updateUser);
+userRoute.delete("/:userId", authMiddleware, deleteUser);
+userRoute.get("/:userId", authMiddleware, getUserById);
 
 exports.userRoute = userRoute;;
